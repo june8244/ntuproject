68270793fbbb7a3353581b1c070823d6ece2136e
chenjingyou <b03901152@ntu.edu.tw>
2016-06-26 19:37:30 +0800

    contact&about&map

---
+++ test_of_parralex/test.js
@@ -0,0 +1,11 @@
+$(window).load(function() {
+    $('#believe').parallax({offsetY:-200}); 
+}
+               
+               
+$(window).load(function() {
+    $('#origing').parallax({offsetY:-300}); 
+}
+
+$('#believe').parallax("50%", 10);
+$('#secorigingond').parallax("50%", 30);
